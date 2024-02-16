21414d048a0987692b5b9cf17059172960d2eb08
ʟᴏᴜɪs <157618892+Louis-X0@users.noreply.github.com>
2024-02-16 14:24:06 +0530

    Create fullpp.js

---
+++ plugins/fullpp.js
@@ -0,0 +1,49 @@
+const { command } = require("../lib/");
+const Jimp = require("jimp");
+
+command(
+  {
+    pattern: "pp$",
+    fromMe: true,
+    desc: "Set full screen profile picture",
+    type: "user",
+  },
+  async (message, match,m) => {
+    if (!message.reply_message.image)
+      return await message.reply("*_Reply to a photo_*");
+    let media = await m.quoted.download();
+    await updateProfilePicture(message.user, media, message);
+    return await message.reply("*Profile Picture Updated*");
+  }
+);
+
+async function updateProfilePicture(jid, imag, message) {
+  const { query } = message.client;
+  const { img } = await generateProfilePicture(imag);
+  await query({
+    tag: "iq",
+    attrs: {
+      to: jid,
+      type: "set",
+      xmlns: "w:profile:picture",
+    },
+    content: [
+      {
+        tag: "picture",
+        attrs: { type: "image" },
+        content: img,
+      },
+    ],
+  });
+}
+
+async function generateProfilePicture(buffer) {
+  const jimp = await Jimp.read(buffer);
+  const min = jimp.getWidth();
+  const max = jimp.getHeight();
+  const cropped = jimp.crop(0, 0, min, max);
+  return {
+    img: await cropped.scaleToFit(324, 720).getBufferAsync(Jimp.MIME_JPEG),
+    preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
+  };
+}
