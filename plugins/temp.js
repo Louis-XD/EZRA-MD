ae72f19a51e91c09a0bf3ce75147b653eaef9681
ʟᴏᴜɪs-xᴅ <157618892+Louis-XD@users.noreply.github.com>
2024-02-19 01:30:58 +0530

    Update temp.js

--- plugins/temp.js
+++ plugins/temp.js
@@ -590,3 +590,101 @@ command(
     await message.client.groupLeave(message.jid)
   }
 );
+
+
+/* Copyright (C) 2024 Louis-X0.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Louis-X0 - Zeta-X0
+*/
+
+command(
+  {
+    pattern: "filter ?(.*)",
+    fromMe: true,
+    desc: Lang.FILTER_DESC,
+    usage: ".filter keyword:message",
+    type: "group",
+  },
+  async (message, match) => {
+    let { prefix } = message;
+    let text, msg;
+    try {
+      [text, msg] = match.split(":");
+    } catch {}
+    if (!match) {
+      filtreler = await getFilter(message.jid);
+      if (filtreler === false) {
+        await message.reply(Lang.NO_FILTER);
+      } else {
+        var mesaj = Lang.FILTERS + "\n\n";
+        filtreler.map(
+          (filter) => (mesaj += `✒ ${filter.dataValues.pattern}\n`)
+        );
+        mesaj += tiny("use : .filter keyword:message\nto set a filter");
+        await message.reply(mesaj);
+      }
+    } else if (!text || !msg) {
+      return await message.reply(
+        "```use : .filter keyword:message\nto set a filter```"
+      );
+    } else {
+      await setFilter(message.jid, text, msg, true);
+      return await message.reply(`_Sucessfully set filter for ${text}_`);
+    }
+  }
+);
+
+
+/* Copyright (C) 2024 Louis-X0.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Louis-X0 - Zeta-X0
+*/
+
+command(
+  {
+    pattern: "stop ?(.*)",
+    fromMe: true,
+    desc: Lang.STOP_DESC,
+    usage: '.stop "hello"',
+    type: "group",
+  },
+  async (message, match) => {
+    if (!match) return await message.reply("\n*Example:* ```.stop hello```");
+
+    del = await deleteFilter(message.jid, match);
+
+    if (!del) {
+      await message.reply(Lang.ALREADY_NO_FILTER);
+    } else {
+      await message.reply(`_Filter ${match} deleted_`);
+    }
+  }
+);
+
+
+command({ on: "text", fromMe: isPrivate }, async (message, match) => {
+  var filtreler = await getFilter(message.jid);
+  if (!filtreler) return;
+  filtreler.map(async (filter) => {
+    pattern = new RegExp(
+      filter.dataValues.regex
+        ? filter.dataValues.pattern
+        : "\\b(" + filter.dataValues.pattern + ")\\b",
+      "gm"
+    );
+    if (pattern.test(match)) {
+      await message.reply(filter.dataValues.text, {
+        quoted: message,
+      });
+    }
+  });
+});
+
+
+/* Copyright (C) 2024 Louis-X0.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Louis-X0 - Zeta-X0
+*/
