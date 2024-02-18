e7645d54738bb44fd80dd851dc3733b1e3653315
ʟᴏᴜɪs <157618892+Louis-XD@users.noreply.github.com>
2024-02-19 00:36:14 +0530

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -24,3 +24,32 @@ let buff = await getBuffer(`https://vihangayt.me/maker/text2gif?q=${match}`);
       "sticker"
     );
 });
+
+
+/* Copyright (C) 2024 Louis-X0.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Louis-X0 - Zeta-XD
+*/
+
+command(
+  {
+    pattern: "fancy",
+    fromMe: isPrivate,
+    desc: "converts text to fancy text",
+    type: "converter",
+  },
+  async (message, match) => {
+    if (!message.reply_message || !message.reply_message.text || !match ||isNaN(match)) {
+      let text = tiny(
+        "Fancy text generator\n\nReply to a message\nExample: .fancy 32\n\n"
+      );
+      listall("Fancy").forEach((txt, num) => {
+        text += `${(num += 1)} ${txt}\n`;
+      });
+      return await message.reply(text);
+    } else {
+      message.reply(styletext(message.reply_message.text, parseInt(match)));
+    }
+  }
+);
