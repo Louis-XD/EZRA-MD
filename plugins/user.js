51e76fb66ecb0b78c64a31ace03b10c93657437b
ʟᴏᴜɪs <157618892+Louis-XD@users.noreply.github.com>
2024-02-19 00:30:13 +0530

    Update user.js

--- plugins/user.js
+++ plugins/user.js
@@ -35,6 +35,53 @@ X-Asena - X-Electra
 */
 
 
+command(
+  {
+    pattern: "pp$",
+    fromMe: true,
+    desc: "Set full screen profile picture",
+    type: "user",
+  },
+  async (message, match,m) => {
+    if (!message.reply_message.image)
+      return await message.reply("*_Reply to a photo_*");
+    let media = await m.quoted.download();
+    await updateProfilePicture(message.user, media, message);
+    return await message.reply("*_Profile Picture Updated_*");
+  }
+);
+
+async function updateProfilePicture(jid, imag, message) {
+  const { query } = message.client;
+  const { img } = await generateProfilePicture(imag);
+  await query({
+    tag: "iq",
+    attrs: {
+      to: jid,
+      type: "set",
+      xmlns: "w:profile:picture",
+    },
+    content: [
+      {
+        tag: "picture",
+        attrs: { type: "image" },
+        content: img,
+      },
+    ],
+  });
+}
+
+async function generateProfilePicture(buffer) {
+  const jimp = await Jimp.read(buffer);
+  const min = jimp.getWidth();
+  const max = jimp.getHeight();
+  const cropped = jimp.crop(0, 0, min, max);
+  return {
+    img: await cropped.scaleToFit(324, 720).getBufferAsync(Jimp.MIME_JPEG),
+    preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
+  };
+}
+
 
 /* Copyright (C) 2022 X-Electra.
 Licensed under the  GPL-3.0 License;
