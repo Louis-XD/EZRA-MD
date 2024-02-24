5629910b2314b3173c4f477967c620a8194a26f5
ʟᴏᴜɪs-xᴅ <157618892+Louis-XD@users.noreply.github.com>
2024-02-24 17:37:17 +0530

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,33 @@
+const { command, isPrivate } = require("../lib/");
+command(
+  {
+    pattern: "owner",
+    fromMe: isPrivate,
+    desc: "owner vcard",
+    type: "user",
+  },
+  async (message, match, m, client) => {
+  const zeta = {
+  'contacts': {
+    'displayName': "𝐋𝐨𝐮𝐢𝐬-𝐗𝐃",
+    'contacts': [{
+      'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN:𝐄𝐙𝐑𝐀-𝐗𝐃\nORG:EZRA-XD BOT;\nTEL;type=CELL;type=VOICE;waid=919747257996:919747257996\nEND:VCARD"
+    }]
+  },
+  'contextInfo': {
+    'externalAdReply': {
+      'title': "𝐄𝐙𝐑𝐀-𝐗𝐃",
+      'body': "𝗘𝗭𝗥𝗔 𝗫𝗗 𝗢𝗪𝗡𝗘𝗥",
+      'thumbnailUrl': "https://i.imgur.com/pae05LQ.jpeg",
+      'mediaType': 0x1,
+      'mediaUrl': " ",
+      'sourceUrl': "http://wa.me/+33757050684?text=Hi+Louis+Im+From+EZRA+XD+Git",
+      'showAdAttribution': true
+    }
+  }
+};
+message.client.sendMessage(message.jid, zeta, {
+  quoted: message
+});
+}
+);
