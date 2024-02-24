949b3ad8ca7abba111f792470dd3eb99faaf8d8b
ʟᴏᴜɪs-xᴅ <157618892+Louis-XD@users.noreply.github.com>
2024-02-24 10:20:48 +0530

    Update searcher.js

--- plugins/searcher.js
+++ plugins/searcher.js
@@ -1,5 +1,7 @@
 const { command, isPrivate } = require("../lib/");
 const fetch = require("node-fetch");
+const axios = require("axios");
+
 command(
     {
         pattern: "sps",
@@ -28,5 +30,60 @@ mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: false,
 thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }}, caption: (txxt)}, {quoted: message })
+    }
+    );
+
+
+
+
+
+command(
+    {
+        pattern: "ginfo",
+        fromMe: isPrivate,
+        desc: "github user details",
+        type: "misc",
+    },
+    async (message, match) => {
+        if (!match) return await message.sendMessage("*_Need Github UserName_*");
+var GHuserInfo = await axios
+          .get(`https://api.github.com/users/${match}`)
+          .then((response) => response.data)
+          .catch((error) => {
+            console.log(error);
+          });
+        let GhUserPP = GHuserInfo.avatar_url;
+        let resText4 = `\n*𝐆𝐈𝐓𝐇𝐔𝐁 𝐔𝐒𝐄𝐑 𝐈𝐍𝐅𝐎*
+
+*Username* : ${GHuserInfo.login}
+*Name* : ${GHuserInfo.name}
+*Bio* : ${GHuserInfo.bio}
+
+> *ID* : ${GHuserInfo.id}
+> *Profile URL* : ${GHuserInfo.html_url}
+> *Type* : ${GHuserInfo.type}
+> *Company* : WhatsApp Bot
+> *Blog* : ${GHuserInfo.blog}
+> *Location* : ${GHuserInfo.location}
+> *Email* : ${GHuserInfo.email}
+> *Twitter* : ${GHuserInfo.twitter_username}
+> *Public Repos* : ${GHuserInfo.public_repos}
+> *Public Gists* : ${GHuserInfo.public_gists}
+> *Followers* : ${GHuserInfo.followers}
+> *Following* : ${GHuserInfo.following}
+> *Account Created At* : ${GHuserInfo.created_at}
+> *Last Updated At* : ${GHuserInfo.updated_at}
+
+𝐄𝐙𝐑𝐀-𝐗𝐃`;
+
+        await message.client.sendMessage(message.jid, {image: {url: GhUserPP, mimetype: "image/jpeg" }, contextInfo: { externalAdReply: {
+title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
+body: "𝙂𝙞𝙩𝙝𝙪𝙗 𝙐𝙨𝙚𝙧 𝙄𝙣𝙛𝙤",
+sourceUrl: "",
+mediaUrl: "",
+mediaType: 1,
+showAdAttribution: true,
+renderLargerThumbnail: false,
+thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }}, caption: (resText4)},{quoted:message})
     }
     );
