0a24c562ffb01f8ea8bf5d4f3ee004e142788f96
ʟᴏᴜɪs-xᴅ <157618892+Louis-XD@users.noreply.github.com>
2024-02-25 23:59:51 +0530

    Update ginfo.js

--- plugins/ginfo.js
+++ plugins/ginfo.js
@@ -8,7 +8,7 @@ command(
         type: "converter",
     },
     async (message, match, client, m) => {
-        if (!match || !match.match(/^https:\/\/chat\.whatsapp\.com\/[a-zA-Z0-9]/)) return await message.reply("Need A WhatsApp Group Link");
+        if (!match || !match.match(/^https:\/\/chat\.whatsapp\.com\/[a-zA-Z0-9]/)) return await message.reply("*_Need A WhatsApp Group Link_*");
 let urlArray = (match).trim().split("/")[3];
 	const metadata = await message.client.groupGetInviteInfo(urlArray)
 const sui = "\n*GROUP INFO*\n\n*id* : " + metadata.id + "\n*title* : " + metadata.subject + "\n*description* : " + metadata.desc + "\n*size* : " + metadata.size + "\n*creator* : " + (metadata.owner ? metadata.owner.split('@')[0] : 'unknown') + "\n*restrict* : " + metadata.restrict + "\n*announce* : " + metadata.announce + "\n*created on* : " + require('moment-timezone')(metadata.creation * 1000).tz('Asia/Kolkata').format('DD/MM/YYYY HH:mm:ss') + "\n\n𝐄𝐙𝐑𝐀-𝐗𝐃";
