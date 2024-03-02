a62565e45760184cc7c30ec91eb4fc27eb1b628e
ʟᴏᴜɪs-xᴅ <157618892+Louis-XD@users.noreply.github.com>
2024-03-03 00:52:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -45,5 +45,5 @@ module.exports = {
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || " ",
   SUDO: process.env.SUDO || "0",
   IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
-  ALIVE_MSG: "Hey I'm Alive Now",
+  ALIVE_MSG: process.env.ALIVE_MSG || "Hey I'm Alive Now",
 };
